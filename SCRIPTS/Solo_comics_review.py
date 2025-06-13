e80eb1fad0a7091011d48539ca677fb3a47b5571
Adriobre <adriobre9@gmail.com>
2025-06-13 18:31:51 +0200

    Update Solo_comics_review.py

--- SCRIPTS/Solo_comics_review.py
+++ SCRIPTS/Solo_comics_review.py
@@ -10,18 +10,19 @@ import ast
 
 # Leer b.json y extraer los book_id que pertenecen a la categoría 'comics, graphic'
 comics_book_ids = set()
-with open('./data_final/goodreads_book_genres_initial.json', 'r', encoding='utf-8') as f:
+with open('.json', 'r', encoding='utf-8') as f:
     for line in f:
-        if line.strip():  # ignoramos líneas vacías
-            # Usamos ast.literal_eval para convertir la cadena a diccionario (ya que se usan comillas simples)
+        if line.strip():  
+            
             registro = ast.literal_eval(line)
-            # Verificamos si en el diccionario 'genres' existe la clave 'comics, graphic'
+            
+            # Verificar si en el diccionario 'genres' existe la clave 'comics, graphic'
             if 'comics, graphic' in registro.get('genres', {}):
                 comics_book_ids.add(registro['book_id'])
 
 # Leer a.json y filtrar las reviews cuyo book_id esté en el conjunto anterior
 reviews_filtradas = []
-with open('./data_final/reviews_english_no_cero.json', 'r', encoding='utf-8') as f:
+with open('.json', 'r', encoding='utf-8') as f:
     for line in f:
         if line.strip():
             review = ast.literal_eval(line)
@@ -31,7 +32,7 @@ with open('./data_final/reviews_english_no_cero.json', 'r', encoding='utf-8') as
 # Guardar las reviews filtradas en un CSV
 with open('.csv', 'w', encoding='utf-8', newline='') as csvfile:
     
-    # Definir los campos que queremos incluir (ajusta según se necesite)
+    # Definir los campos a incluir (ajusta según se necesite)
     fieldnames = ['user_id', 'book_id', 'rating', 'review_text', 'date_added']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
